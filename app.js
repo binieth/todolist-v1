b80426999d01edbdbb5ec4dfb1e1c05e35009a32
binieth <abigiyabiniyam@gmail.com>
2019-07-30 09:26:31 -0400

    fixed the bug of creating and adding item to a new page

--- app.js
+++ app.js
@@ -4,22 +4,22 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-let items = ['Buy Food', 'Cook Food', 'Eat Food']; 
+let items = ['Wash', 'Brew Coffee', 'Read']; 
 let workItems = [];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.get("/", function (req, res) {
 
     let today = new Date();
+   
     let options = {
         weekday: 'long',
         day: 'numeric',
         month: 'long'
-
     };
 
     let day = today.toLocaleDateString('en-US', options);
@@ -28,20 +28,18 @@ app.get("/", function (req, res) {
 
 });
 
-app.post("/", function(req, res){
-    // console.log(req.body);
-    
-    let item = req.body.newItem;
-    // items.push(item);
-    // res.redirect("/");
 
-    if(req.body.list === "Work"){
+app.post("/", function(req, res){
+    let item = req.body.newItem;
+  
+    if (req.body.list === "Work"){
         workItems.push(item);
         res.redirect("/work");
     } else{
         items.push(item);
         res.redirect("/");
     }
+    
 
 });
 
@@ -50,14 +48,11 @@ app.get("/work", function(req, res) {
     res.render("list", {listTitle:"Work List", newListItems: workItems});
 });
 
-// app.post("/work", function(req, res){
-//     let item = req.body.newItem;
-//     workItems.push(item);
-//     res.redirect("/work");
-
-// });
-
-
+app.post("/work", function(req, res){
+    let item = req.body.newItem;
+    workItems.push(item);
+    res.redirect("/work");
+})
 app.listen(3000, function () {
     console.log('the application is running on port 3000.')
 });
